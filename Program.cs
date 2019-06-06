a41eebc7b0f0831bdbdc033745e85f049fc35a96
Gisela Torres <giselatb@outlook.com>
2019-06-06 19:34:45 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Logging;
+
+namespace AppConfigurationDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+             .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var settings = config.Build();
+                config.AddAzureAppConfiguration(options =>
+               {
+                   options.Connect(settings["ConnectionStrings:AppConfiguration"]).UseFeatureFlags().WatchAndReloadAll("Sentinel", TimeSpan.FromSeconds(5));
+               });
+            })
+            .UseStartup<Startup>();
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(evt) {
+      evt.preventDefault();
+      
+      var link = $(this).attr('href');
+      var less = (!$('#nav').hasClass('active')) ? 50 : 0;
+      
+      $('html, body').animate({
+        scrollTop: $(link).offset().top - less
+      }, 750);
+    });
+    
+    $(window).on('scroll', function() {
+      if ($(this).scrollTop() > 100) {
+        $('#nav').addClass('active');
+      }
+      else {
+        $('#nav').removeClass('active');
+      }
+    });
+  });  
